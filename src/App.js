4b199592f4224553f1c11868a3240e2453d489f6
Stein Fletcher <steindfletcher@gmail.com>
2021-07-13 10:23:36 +0100

    Fix compiler error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
         <picture>
           <source srcSet="https://assets.sainsburys-groceries.co.uk/gol/8054599/image.webp" alt="" type="image/webp"/>
           <source srcSet="https://assets.sainsburys-groceries.co.uk/gol/8054599/image.jpg" alt="" type="image/jpeg"/>
-          <img src="https://assets.sainsburys-groceries.co.uk/gol/8054599/image.jpg" alt="" alt="Alt Text!"/>
+          <img src="https://assets.sainsburys-groceries.co.uk/gol/8054599/image.jpg" alt="Alt Text!"/>
         </picture>
       </div>
   );
