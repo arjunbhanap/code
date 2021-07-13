c48fdda7186d0c7234f080c5e62d4e89934eaffe
Arjun Bhanap <arjunbhanap@Arjuns-MacBook-Air.local>
2021-07-13 14:41:00 +0100

    make sku dynamic on product details page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 function App() {
+  const url = new URLSearchParams(window.location.search)
+  const sku = url.get("sku")
+  console.log(sku);
   return (
       <div className="App">
         <picture>
-          <source srcSet="https://assets.sainsburys-groceries.co.uk/gol/8054599/image.webp" alt="" type="image/webp"/>
-          <source srcSet="https://assets.sainsburys-groceries.co.uk/gol/8054599/image.jpg" alt="" type="image/jpeg"/>
-          <img src="https://assets.sainsburys-groceries.co.uk/gol/8054599/image.jpg" alt="Alt Text!"/>
+          <source srcSet={`https://assets.sainsburys-groceries.co.uk/gol/${sku}/image.webp`} alt="" type="image/webp"/>
+          <source srcSet={`https://assets.sainsburys-groceries.co.uk/gol/${sku}/image.jpg`} alt="" type="image/jpeg"/>
+          <img src={`https://assets.sainsburys-groceries.co.uk/gol/${sku}/image.jpg`} alt="Alt Text!"/>
         </picture>
       </div>
   );
