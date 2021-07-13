55c937437b1f667758f886b4e8ddfaf7120e6d04
Stein Fletcher <steindfletcher@gmail.com>
2021-07-13 10:13:03 +0100

    Initial code

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <picture>
+          <source srcSet="https://assets.sainsburys-groceries.co.uk/gol/8054599/image.webp" alt="" type="image/webp"/>
+          <source srcSet="https://assets.sainsburys-groceries.co.uk/gol/8054599/image.jpg" alt="" type="image/jpeg"/>
+          <img src="https://assets.sainsburys-groceries.co.uk/gol/8054599/image.jpg" alt="" alt="Alt Text!"/>
+        </picture>
+      </div>
+  );
+}
+
+export default App;
